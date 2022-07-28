6acf497e9f2644d101e2a859117d6933a1618c98
Qa-krushna <rashwini510@gmail.com>
2022-07-28 22:07:09 +0530

    Added: Ability to connect database ti java program and retrieve all data from database

--- src/main/java/com/bridgelabz/AddressBookService.java
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -1,7 +1,29 @@
 package com.bridgelabz;
 
+import java.sql.*;
+
 public class AddressBookService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book service jdbc");
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Status of connecting database to java code: ");
+        String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service";
+        String userName = "root";
+        String password = "password";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection done!!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM address_book");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("firstName") + " | " + resultSet.getString("lastName") + " | " + resultSet.getString("address") + " | " + resultSet.getString("city") + " | " + resultSet.getString("state") + " | " + resultSet.getInt("zip") + " | " + resultSet.getInt("phone_Number") + " | " + resultSet.getString("email_id") + " | " + resultSet.getString("bookName") + " | " + resultSet.getString("contactType"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 }
+
